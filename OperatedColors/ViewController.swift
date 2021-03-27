d94f51dd723cdd6a3d4c9ec4c8e0618a0675f0d3
Evgeniy Kostin <kostinen@icloud.com>
2021-03-28 00:58:13 +0300

    preview 1.0

--- OperatedColors/ViewController.swift
+++ OperatedColors/ViewController.swift
@@ -9,11 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var screenColorsView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var valueRedLabel: UILabel!
+    @IBOutlet weak var valueGreenLabel: UILabel!
+    @IBOutlet weak var valueBlueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    /*
+     @IBOutlet weak var redLabel: UILabel!
+     @IBOutlet weak var greenLabel: UILabel!
+     @IBOutlet weak var blueLabel: UILabel!
+     
+     @IBOutlet weak var valueRedLabel: UILabel!
+     @IBOutlet weak var valueGreenLabel: UILabel!
+     @IBOutlet weak var valueBlueLabel: UILabel!
+     
+     @IBOutlet weak var redSlider: UISlider!
+     @IBOutlet weak var greenSlider: UISlider!
+     @IBOutlet weak var blueSlider: UISlider!
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        valueRedLabel.text = ""
+        valueRedLabel.font = valueRedLabel.font.withSize(22)
+        valueGreenLabel.text = ""
+        valueGreenLabel.font = valueGreenLabel.font.withSize(22)
+        valueBlueLabel.text = ""
+        valueBlueLabel.font = valueBlueLabel.font.withSize(22)
+        
+        screenColorsView.layer.cornerRadius = 10
+        
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        
+        redSlider.thumbTintColor = .red
+        greenSlider.thumbTintColor = .green
+        blueSlider.thumbTintColor = .blue
+        
+        redSlider.value = 0.1
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.value = 0.2
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.value = 0.3
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        valueRedLabel.text = String(redSlider.value)
+        valueGreenLabel.text = String(greenSlider.value)
+        valueBlueLabel.text = String(blueSlider.value)
     }
 
-
+    @IBAction func sliderActionForRed() {
+        valueRedLabel.text = String(redSlider.value)
+    }
+    @IBAction func sliderActionForGreen() {
+        valueGreenLabel.text = String(greenSlider.value)
+    }
+    @IBAction func sliderActionForBlue() {
+        valueBlueLabel.text = String(blueSlider.value)
+    }
 }
 
